018ea6d44d9a3a57405a2d04afc849c07d60e123
Harika_Jeedigunta <84392557+Harikajeedigunta@users.noreply.github.com>
2022-10-14 11:55:35 +0530

    Add files via upload

---
+++ ex5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int balls=15;
+  cin>>balls;
+  if(balls%2==0)
+  {
+  	cout<<"ashok balls";
+  }
+  else if((balls%2==0)and(balls%2==0>balls%2!=0))
+  {
+  	cout<<"\nashok balls";
+  	cout<<"\nashok will win";
+  }
+  else
+  {
+  	cout<<"\ntivitt balls";
+  	cout<<"\ntivitt will win";
+  }
+  
+  return 0;
+}
