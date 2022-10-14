62616b0c816280a9c9c56da6d58c0a206fdf9efc
Harika_Jeedigunta <84392557+Harikajeedigunta@users.noreply.github.com>
2022-10-14 11:59:18 +0530

    Add files via upload

---
+++ ex7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int table;
+	int range;
+	cin>>table>>range;
+	while(int i=1)
+	{
+	i++;
+	i<=range;
+	cout<<table<<"x"<<i<<"="<<table*i<<endl;
+	break;
+    }
+
+return 0;
+}
