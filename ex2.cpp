a2625e991254a65a550f9d9f9b13a311eddd6ba3
Harika_Jeedigunta <84392557+Harikajeedigunta@users.noreply.github.com>
2022-10-14 11:53:42 +0530

    Add files via upload

---
+++ ex2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	if(n>=1000)
+	{
+		cout<<"\nbunk the college";
+		cout<<"\n party";
+		cout<<"\n movie";
+	}
+	else if(n>=500)
+	{
+		cout<<"\nbunk";
+		cout<<"\nparty";
+	}
+	else if(n=100)
+	{
+		cout<<"crying and coming to college";
+	}
+	return 0;
+	
+}
