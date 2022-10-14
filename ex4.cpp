b022db68e3a09731327a1606d2410de0971a3a70
Harika_Jeedigunta <84392557+Harikajeedigunta@users.noreply.github.com>
2022-10-14 11:55:06 +0530

    Add files via upload

---
+++ ex4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int fee;
+ cin>>fee;
+ if(become coder satus==1)
+ {
+ 	cout<<"eligible for 5% fee consition";
+ }
+ else
+ {
+ 	cout<<"have to pay full fee";
+ } return 0;
+}
+
